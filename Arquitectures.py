a5aa1a4c5a0e4f8ab8c2b668baa210e3629d7d5a
Uriel Rubio <privuri@gmail.com>
2024-07-05 00:04:13 +0200

    Added Neuron class

--- Arquitectures.py
+++ Arquitectures.py
@@ -17,10 +17,11 @@ from numpy import (
     uint8,
 )
 
-verify_type = Verifiers.verify_type 
+verify_type = Verifiers.verify_type
 verify_len = Verifiers.verify_len
 verify_iterable = Verifiers.verify_iterable
 verify_components_type = Verifiers.verify_components_type
+
 
 class Perceptron:
     "Class representing a Perceptron (Unitary Layer Neural DL Model)"
@@ -72,7 +73,6 @@ class Perceptron:
 
     def __call__(self, X: Union[list, ndarray]) -> int:
         return self.predict(X)
-
 
     @property
     def id(self) -> int:
@@ -165,21 +165,25 @@ class Perceptron:
                 for i in range(len(self._weights)):
                     self._weights[i] += self._lr * (ey - z) * eX[i]
                 self._bias += self._lr * (ey - z)
-            
+
             # Calculate loss MSE for the current epoch
-            epoch_loss = sum((self._y[i] - self.__call__(self._X[i * self._n : (i + 1) * self._n])) ** 2 for i in range(len(self._y))) / len(self._y)
+            epoch_loss = sum(
+                (self._y[i] - self.__call__(self._X[i * self._n : (i + 1) * self._n]))
+                ** 2
+                for i in range(len(self._y))
+            ) / len(self._y)
             history.append(epoch_loss)
 
             if verbose:
-                print(f"Epoch {epoch}:\n\tModel output: {z}\n\tExpected output: {ey}\n\tLoss: {epoch_loss}")
+                print(
+                    f"Epoch {epoch}:\n\tModel output: {z}\n\tExpected output: {ey}\n\tLoss: {epoch_loss}"
+                )
 
         return history
 
     def predict(self, X: Union[list, ndarray]) -> int:
         """Returns a prediction given X as inputs."""
-        verify_len(
-            X, self._n  # The input must be the same shape as n.
-        )
+        verify_len(X, self._n)  # The input must be the same shape as n.
         verify_components_type(
             X, (int, float, *Perceptron.__nptypes)  # Input data must be numeric.
         )
@@ -187,3 +191,76 @@ class Perceptron:
         return Perceptron.step(
             sum(x * w for x, w in zip(X, self._weights)) + self._bias
         )
+
+
+class Neuron:
+    """Class representing an artificial neuron"""
+
+    __nptypes = (
+        int8,
+        int16,
+        int32,
+        int64,
+        uint8,
+        uint16,
+        uint32,
+        uint64,
+        float16,
+        float32,
+        float64,
+    )
+
+    def __init__(self) -> None:
+        self._bias: float = random()
+        self._inputs: list = []
+        self._weights: list = [random() for _ in range(len(self._inputs))]
+        self._activation: function = self.__step
+        self._z = self._activation(
+            sum(x * w for x, w in zip(self._inputs, self._weights)) + self._bias
+        )
+
+        self._layer: int
+        self._i: int
+
+    def __step(self, x: Union[int, float]) -> int:
+        verify_type(x, (int, float, *Neuron.__nptypes))
+        return 1 if x >= 0 else 0
+
+    @property
+    def b(self) -> float:
+        """The bias property."""
+        return self._bias
+
+    @b.setter
+    def b(self, value: Union[int, float]) -> None:
+        self._bias = float(verify_type(value, (int, float, *Neuron.__nptypes)))
+
+    @property
+    def inputs(self) -> list:
+        """The inputs property."""
+        return self._inputs
+
+    @inputs.setter
+    def inputs(self, value: list) -> None:
+        self._inputs = verify_type(value, list)
+
+    @property
+    def w(self) -> list:
+        """The w property."""
+        return self._w
+
+    @w.setter
+    def w(self, value) -> None:
+        self._w = verify_type(value, (list, ndarray))
+
+    @property
+    def activation(self) -> function:
+        """The activation property."""
+        return self._activation
+
+    @activation.setter
+    def activation(self, value: function) -> None:
+        if not callable(value):
+            raise TypeError("Expected value to be callable.")
+
+        self._activation = value
--- testing.py
+++ testing.py
@@ -12,7 +12,7 @@ and_y: list = [int(bool(X[i]) and bool(X[i + 1])) for i in range(0, len(X) - 1, 
 
 and_g = Perceptron(X, and_y, ENTRIES)
 
-print("Loss history for AND", and_g.train(verbose=True))
+print("\nLoss history for AND", and_g.train(verbose=True))
 print("AND weights", and_g._weights)
 print("AND bias", and_g._bias)
 
@@ -31,7 +31,7 @@ or_y: list = [int(bool(X[i]) or bool(X[i + 1])) for i in range(0, len(X) - 1, 2)
 
 or_g = Perceptron(X, or_y, ENTRIES)
 
-print("Loss history for OR", or_g.train())
+print("\nLoss history for OR", or_g.train())
 print("OR weights", or_g._weights)
 print("OR bias", or_g._bias)
 
@@ -40,7 +40,7 @@ nand_y: list = [int(not (bool(X[i]) and bool(X[i + 1]))) for i in range(0, len(X
 
 nand_g = Perceptron(X, nand_y, ENTRIES)
 
-print("Loss history for NAND", nand_g.train())
+print("\nLoss history for NAND", nand_g.train())
 print("NAND weights", nand_g._weights)
 print("NAND bias", nand_g._bias)
 
@@ -49,7 +49,7 @@ xor_y: list = [int(bool(X[i]) ^ bool(X[i + 1])) for i in range(0, len(X) - 1, 2)
 
 xor_g = Perceptron(X, xor_y, ENTRIES)
 
-print("Loss history for XOR", xor_g.train())
+print("\nLoss history for XOR", xor_g.train())
 print("XOR weights", xor_g._weights)
 print("XOR bias", xor_g._bias)
 
@@ -61,6 +61,6 @@ not_y: list = [int(not bool(X[i])) for i in range(len(X))]
 
 not_g = Perceptron(X, not_y, ENTRIES)
 
-print("Loss history for NOT", not_g.train())
+print("\nLoss history for NOT", not_g.train())
 print("NOT weights", not_g._weights)
 print("NOT bias", not_g._bias)
